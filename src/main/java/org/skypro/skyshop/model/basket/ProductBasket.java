26ba32a0ea73667c3992171f7f79578870c59a2a
Kail-Stunt <Kail-Stunt@mail.ru>
2026-03-19 23:50:30 +0700

    Готовое домашнее задание

---
+++ src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
@@ -0,0 +1,21 @@
+package org.skypro.skyshop.model.basket;
+
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+@Service
+public class ProductBasket {
+    private final Map<UUID, Integer> products;
+
+    public ProductBasket() {
+        this.products = new HashMap<>();
+    }
+
+    public void addProduct(UUID productId) {
+        products.merge(productId,1,Integer::sum);
+    }
+
+    public Map<UUID, Integer> getProducts() {
+        return Collections.unmodifiableMap(products);
+    }
+}
